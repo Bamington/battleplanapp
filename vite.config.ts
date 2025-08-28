b2ef3442b7fbe1dd94949c47e33cc9ea9ac23d2c
Bamington <chris.bam.harrison@gmail.com>
2025-08-28 15:51:52 +1000

    Changes to accomodate Vercel deployments.

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,6 @@ export default defineConfig({
   server: {
     historyApiFallback: true,
   },
-  base: process.env.NODE_ENV === 'production' ? '/battleplanapp/' : '/',
+  // Remove the base path for Vercel deployment
+  // base: process.env.NODE_ENV === 'production' ? '/battleplanapp/' : '/',
 });
